c77ba0d4ef6d3f48a04678e0e57e0c6cdfb88560
Barthe8061D <barthelapeyrigne.aurelien@gmail.com>
2017-11-09 17:39:33 +0100

    First contract's version

---
+++ contracts/FriendBet.sol
@@ -0,0 +1,77 @@
+pragma solidity ^0.4.2;
+
+contract Bethereum{
+    address _creator;
+    uint public _valueBet;
+    uint _endTimeBet;
+    uint _endMatch;
+    uint _balance;
+    uint _team1Bet;
+    uint _team2Bet;
+    uint _team3Bet;
+    uint _winner;
+    uint _prizeBet;
+
+    mapping(address=>uint) public playerChoice;
+    mapping(uint => uint) public numberOfBetForTeam;
+
+    event Finished (uint winner);
+
+
+    //--------------------modifier-----------------------------
+    modifier isOwner() {
+        if(msg.sender != _creator) return;
+        _;
+    }
+
+    modifier notPlayedYet(){
+      if(playerChoice[msg.sender] != 0) return;
+      _;
+    }
+
+    modifier canStillBet() {
+      if (now >= _endTimeBet) return;
+      _;
+    }
+
+    modifier endOfMatch(){
+      if(now < _endMatch) return;
+      _;
+    }
+
+
+    function Bethereum(uint valueBet, uint endMatch, uint endTimeBet, uint team) public {
+        _valueBet = valueBet;
+        _endTimeBet = endTimeBet;
+        _endMatch = endMatch;
+        _creator = msg.sender;
+        _balance = 0
+        numberOfBetForTeam[3] = 0; //if bet on draw
+        numberOfBetForTeam[1] = 0;
+        numberOfBetForTeam[2] = 0;
+        _winner = 0;
+        _prizeBet = 0;
+
+        betTeam(team);
+    }
+
+    function betTeam(uint team) notPlayedYet() {
+        playerChoice[msg.sender] = team;
+        _balance += _valueBet;
+        numberOfBetForTeam[team] += 1;
+    }
+    function getNumberOFBetForTeam(uint team) public constant returns(uint){
+      return(numberOfBetForTeam[team]);
+    }
+
+    function getwinnerBet(uint winner) isOwner(){
+      _winner = winner;
+      _prizeBet = _valueBet*(numberOfBetForTeam[3]+ numberOfBetForTeam[1]+ numberOfBetForTeam [2])/ numberOfBetForTeam[winner];
+    }
+    function getmoneybet(){
+      playerChoice[msg.sender] = 3; //avoid withdraw more than once
+      msg.sender.transfer(_prizeBet);
+    }
+
+
+}
